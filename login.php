0ee9a38f7302ecefafa164d8ac1aade5b8fa6625
“rahmad310317” <rahmadwindisimanullang@gmail.com>
2025-02-05 04:16:09 +0700

    update login

--- login.php
+++ login.php
@@ -8,7 +8,13 @@
 </head>
 
 <body>
-
+   <form action="login.php" method="post">
+      <label for="username">Username</label>
+      <input type="text" name="username" id="username">
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password">
+      <button type="submit">Login</button>
+   </form>
 </body>
 
 </html>
