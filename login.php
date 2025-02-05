395ee47b659087a005d4bd71359b97827f2145a3
“rahmad310317” <rahmadwindisimanullang@gmail.com>
2025-02-06 01:47:20 +0700

    update logic

--- login.php
+++ login.php
@@ -8,12 +8,30 @@
 </head>
 
 <body>
+   <?php
+   include_once "./koneksi.php";
+
+   if (isset($_POST['submit'])) {
+      $username = mysqli_real_escape_string($conn, $_POST['username']);
+      $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+      $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+      $result = mysqli_query($conn, $query);
+
+      if (mysqli_num_rows($result) > 0) {
+         echo "Login berhasil";
+      } else {
+         echo "Login gagal";
+      }
+   }
+   ?>
+
    <form action="login.php" method="post">
       <label for="username">Username</label>
-      <input type="text" name="username" id="username">
+      <input type="text" name="username" id="username" required>
       <label for="password">Password</label>
-      <input type="password" name="password" id="password">
-      <button type="submit">Login</button>
+      <input type="password" name="password" id="password" required>
+      <button type="submit" name="submit">Login</button>
    </form>
 </body>
 
